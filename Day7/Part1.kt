a92561facf0d169f5b1da019506f6146a8a53929
JacobGirling98 <jacob.girling98@gmail.com>
2021-12-07 22:06:55 +0000

    day 7 refactoring

--- Day7/Part1.kt
+++ Day7/Part1.kt
@@ -1,11 +1,10 @@
 import java.io.File
-import java.lang.Math.abs
 
 
 class LaserCrab() {
 
-    var crabs: List<Int> = listOf()
-    var median: Int = 0
+    private var crabs: List<Int> = listOf()
+    private var median: Int = 0
 
     fun readFile(fileName: String) {
         crabs = File(fileName).useLines { it.first().split(",").map { it.toInt() }.sorted() }
@@ -13,26 +12,21 @@ class LaserCrab() {
 
     fun findMedian() {
         val mid: Int = crabs.size / 2
-        if (crabs.size % 2 == 0) {
-            median = (crabs[mid] - crabs[mid - 1]) / 2 + crabs[mid - 1]
+        median = if (crabs.size % 2 == 0) {
+            (crabs[mid] - crabs[mid - 1]) / 2 + crabs[mid - 1]
         } else {
-            median = crabs[mid]
+            crabs[mid]
         }
     }
 
     fun calculateLeastFuel(): Int {
-        var fuel = 0
-        for (crab in crabs) {
-            fuel += abs(crab - median)
-        }
-        return fuel
+        return crabs.sumOf { kotlin.math.abs(it - median) }
     }
 }
 
-fun main(args: Array<String>) {
+fun main() {
     val laser = LaserCrab()
     laser.readFile("src/main/kotlin/crabs.txt")
+    laser.findMedian()
     println(laser.calculateLeastFuel())
-
-
 }
--- Day7/Part2.kt
+++ Day7/Part2.kt
@@ -4,48 +4,30 @@ import kotlin.math.roundToInt
 
 class LaserCrab() {
 
-    var crabs: List<Int> = listOf()
-    var mean: Int = 0
+    private var crabs: List<Int> = listOf()
 
     fun readFile(fileName: String) {
         crabs = File(fileName).useLines { it.first().split(",").map { it.toInt() }.sorted() }
     }
 
-    fun findMean() {
-        mean = (crabs.sum().toFloat() / crabs.size).roundToInt()
+    fun findMean(): Int {
+        return (crabs.sum().toFloat() / crabs.size).roundToInt()
     }
 
-    fun calculateLeastFuel(): Int {
-        var fuel = 0
-        var distance: Int
-        for (crab in crabs) {
-            distance = kotlin.math.abs(crab - mean)
-            for (delta in 1..distance) {
-                fuel += delta
-            }
-        }
-        return fuel
+    private fun calculateLeastFuel(mean: Int): Int {
+        return crabs.sumOf { (1..kotlin.math.abs(it - mean)).sum() }
     }
 
-    fun plusMinusMean(delta: Int, initialValue: Int, initialMean: Int): Int {
-        var minimumFuel: Int = initialValue;
-        var newFuel = 0;
-        for (m in (initialMean - delta)..(initialMean + delta)) {
-            mean = m
-            newFuel = calculateLeastFuel();
-            if (newFuel < minimumFuel) {
-                minimumFuel = newFuel
-            }
-        }
-        return minimumFuel
+    // not sure why min may give a null here
+    fun searchLeastFuel(delta: Int, initialMean: Int): Int? {
+        return ((initialMean - delta)..(initialMean + delta)).map { calculateLeastFuel(it) }.minOrNull()
     }
 }
 
 fun main() {
     val crabArmy = LaserCrab()
     crabArmy.readFile("src/main/kotlin/crabs.txt")
-    crabArmy.findMean()
-    val initialMin = crabArmy.calculateLeastFuel()
+    val initialMean = crabArmy.findMean();
     // hate that I have to brute force this - the actual min value isn't the mean!!!
-    println(crabArmy.plusMinusMean(5, initialMin, crabArmy.mean))
+    println(crabArmy.searchLeastFuel(5, initialMean))
 }
